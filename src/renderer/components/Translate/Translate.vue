8fe6e64c966782000b92cdd573b12577b191e84d
khanht <khanh.t@citigo.com.vn>
2019-07-04 18:28:50 +0700

    update

--- src/renderer/components/Translate/Translate.vue
+++ src/renderer/components/Translate/Translate.vue
@@ -1,39 +1,53 @@
 <template>
   <div id="wrapper">
-    <h3>Translate</h3>
-    <b-form-textarea
-      class="mt-3"
-      id="textarea"
-      v-model="inputText"
-      placeholder="Nhập từ cần dịch ..."
-      rows="3"
-      max-rows="6"
-    ></b-form-textarea>
-    <b-button
-      class="mt-3 mb-3"
-      @click="transalte"
-      variant="primary"
-    >Dịch</b-button>
-    <b-form-textarea
-      id="textarea"
-      v-model="resultText"
-      placeholder="Kết quả ..."
-      rows="3"
-      max-rows="6"
-      :disabled=true
-      style="cursor: not-allowed"
-      oncopy="return false"
-    ></b-form-textarea>
-    <div>
-    <b-button
-      class="mt-3 mb-3"
-      @click="copy"
-      variant="danger"
-    >Copy</b-button>
-    </div>
-    <div class="footer mt-3" style="float: right">
-      <small>version 1.0.1</small>
-    </div>
+    <b-container fluid>
+      <h3>Translate</h3>
+      <b-row>
+		  
+	  </b-row>
+	  <b-col md="5">
+        <b-form-textarea
+          class="mt-3"
+          id="textarea"
+          v-model="inputText"
+          placeholder="Nhập từ cần dịch ..."
+          rows="3"
+          max-rows="6"
+        ></b-form-textarea>
+        <b-button
+          class="mt-3 mb-3"
+          @click="transalte"
+          variant="primary"
+        >Dịch</b-button>
+      </b-col>
+      <b-col md="5">
+        <b-form-textarea
+          id="textarea"
+          v-model="resultText"
+          placeholder="Kết quả ..."
+          rows="3"
+          max-rows="6"
+          :disabled=true
+          style="cursor: not-allowed"
+          oncopy="return false"
+        ></b-form-textarea>
+        <div>
+          <b-button
+            class="mt-3 mb-3"
+            @click="copy"
+            variant="danger"
+          >Copy</b-button>
+        </div>
+      </b-col>
+
+      <div
+        class="footer mt-3"
+        style="float: right"
+      >
+        <small>version 1.0.1</small>
+      </div>
+    </b-container>
+
   </div>
 
 </template>
