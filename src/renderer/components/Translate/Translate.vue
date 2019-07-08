5cdfb8ce82e37a6deb123f286b801186bdfc3c03
khanht <khanht.dev@gmail.com>
2019-07-08 07:31:09 +0700

    update

--- src/renderer/components/Translate/Translate.vue
+++ src/renderer/components/Translate/Translate.vue
@@ -3,10 +3,7 @@
     <b-container fluid>
       <h3>YunXi Auto</h3>
       <b-row class="mt-3">
-        <b-col
-          md="5"
-          class="mr-3"
-        >
+        <b-col md="5" class="mr-3">
           <b-form-textarea
             id="textarea"
             v-model="inputText"
@@ -16,11 +13,7 @@
             @input="transalte"
           ></b-form-textarea>
           <div style="text-align: center">
-            <b-button
-              class="mt-3 mb-3"
-              @click="clearInput"
-              variant="primary"
-            >Xóa</b-button>
+            <b-button class="mt-3 mb-3" @click="clearInput" variant="primary">Xóa</b-button>
           </div>
         </b-col>
         <b-col md="5">
@@ -30,30 +23,21 @@
             placeholder="Kết quả ..."
             rows="15"
             max-rows="15"
-            :disabled=true
+            :disabled="true"
             style="cursor: not-allowed"
             oncopy="return false"
           ></b-form-textarea>
           <div>
-            <b-button
-              class="mt-3 mb-3 mr-2"
-              @click="copy"
-              variant="danger"
-            >Kết quả</b-button>
-            <b-button
-              class="mt-3 mb-3"
-              @click="clearResult"
-              variant="primary"
-            >Xóa</b-button>
+            <b-button class="mt-3 mb-3 mr-2" @click="copy" variant="danger">Kết quả</b-button>
+            <b-button class="mt-3 mb-3" @click="clearResult" variant="primary">Xóa</b-button>
           </div>
         </b-col>
       </b-row>
-      <div
-        class="footer"
-        style="float: right; position: relative; top: -55px;"
-      >
+      <div class="footer" style="float: right; position: relative; top: -55px;">
         <small style="display: block">Version 1.01</small>
-        <p><small>093.595.0000</small></p>
+        <p>
+          <small>093.595.0000</small>
+        </p>
       </div>
     </b-container>
   </div>
@@ -61,69 +45,81 @@
 
 <script>
 import ApiService from "../../services/Api";
-import Config from './config'
-const { clipboard } = require('electron')
-const jsonConfig = require('electron-json-config');
+import Config from "./config";
+const { clipboard } = require("electron");
+const jsonConfig = require("electron-json-config");
 export default {
-  name: 'translate',
-  data () {
+  name: "translate",
+  data() {
     return {
       translateData: [],
-      inputText: '',
-      resultText: ''
-    }
+      inputText: "",
+      resultText: ""
+    };
   },
   methods: {
-    clearInput () {
-      this.inputText = ""
+    clearInput() {
+      this.inputText = "";
     },
-    clearResult () {
-      this.resultText = ""
+    clearResult() {
+      this.resultText = "";
     },
-    transalte () {
-      this.resultText = ''
-      const inputTexts = this.inputText.split('\n')
+    transalte() {
+      this.resultText = "";
+      // get array text with \n
+      const inputTexts = this.inputText.split("\n");
+
       inputTexts.map(inputText => {
-        const position = inputText.trim().search(/[0-9]/gm)
-        let text, number
+        const position = inputText.trim().search(/[0-9]/gm);
+        let text, number;
         if (position !== -1) {
-          text = inputText.slice(0, position)
-          number = inputText.slice(position, inputText.length)
+          text = inputText.slice(0, position);
+          number = inputText.slice(position, inputText.length);
         } else {
-          text = inputText
-          number = ''
+          text = inputText;
+          number = "";
         }
 
-        let textTranslate = this.translateData[text.trim()] ? this.translateData[text.trim()] : text.trim()
-        const numberTransalte = number.slice(number.search('_') + 1, number.length)
+        let textTranslate = this.translateData[text.trim()]
+          ? this.translateData[text.trim()]
+          : text.trim();
+        const numberTransalte = number.slice(
+          number.search("_") + 1,
+          number.length
+        );
+
         if (textTranslate) {
-          textTranslate = textTranslate.replace("柜体","");
-          this.resultText += textTranslate
-          if(numberTransalte){
-            this.resultText += "\t" + numberTransalte + '\n'
-          }else{
-            this.resultText += "\n"
+          textTranslate = textTranslate.replace("柜体", "");
+          this.resultText += textTranslate;
+          if (numberTransalte) {
+            if (number.search("_") !== -1) {
+              this.resultText += "\t" + numberTransalte + "\n";
+            } else {
+              this.resultText += numberTransalte + "\n";
+            }
+          } else {
+            this.resultText += "\n";
           }
         } else {
-          this.resultText += numberTransalte + '\n'
+          this.resultText += numberTransalte + "\n";
         }
-      })
-        this.resultText = this.resultText.trim();
+      });
+      this.resultText = this.resultText.trim();
     },
-    copy () {
-      clipboard.writeText(this.resultText)
+    copy() {
+      clipboard.writeText(this.resultText);
     }
   },
-  mounted () {
-    ApiService.get(
-      Config.url
-    ).then(response => {
-      jsonConfig.set('dataTranslate', response.data)
-    }).catch(error => {
-      console.log(error)
-    })
+  mounted() {
+    ApiService.get(Config.url)
+      .then(response => {
+        jsonConfig.set("dataTranslate", response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
 
-    this.translateData = jsonConfig.get('dataTranslate');
+    this.translateData = jsonConfig.get("dataTranslate");
   }
-}
+};
 </script>
