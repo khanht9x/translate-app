78c83d80f13b0021361e8e8e4a558736a80d5089
khanht <khanht.com>
2021-10-09 16:08:30 +0700

    update

--- src/renderer/components/Translate/Translate.vue
+++ src/renderer/components/Translate/Translate.vue
@@ -13,10 +13,7 @@
         </b-col>
       </b-row>
       <b-row class="mt-3 token">
-        <b-col
-          md="5"
-          class="mr-3"
-        >
+        <b-col md="5" class="mr-3">
           <b-form-textarea
             id="textarea"
             v-model="inputText"
@@ -26,11 +23,9 @@
             @input="transalte"
           ></b-form-textarea>
           <div style="text-align: center">
-            <b-button
-              class="mt-3 mb-3"
-              @click="clearInput"
-              variant="primary"
-            >Xóa</b-button>
+            <b-button class="mt-3 mb-3" @click="clearInput" variant="primary"
+              >Xóa</b-button
+            >
           </div>
         </b-col>
         <b-col md="5">
@@ -45,23 +40,16 @@
             oncopy="return false"
           ></b-form-textarea>
           <div>
-            <b-button
-              class="mt-3 mb-3 mr-2"
-              @click="copy"
-              variant="danger"
-            >Kết quả</b-button>
-            <b-button
-              class="mt-3 mb-3"
-              @click="clearResult"
-              variant="primary"
-            >Xóa</b-button>
+            <b-button class="mt-3 mb-3 mr-2" @click="copy" variant="danger"
+              >Kết quả</b-button
+            >
+            <b-button class="mt-3 mb-3" @click="clearResult" variant="primary"
+              >Xóa</b-button
+            >
           </div>
         </b-col>
       </b-row>
-      <div
-        class="footer float-right"
-        style="position: relative; top: -55px;"
-      >
+      <div class="footer float-right" style="position: relative; top: -55px">
         <small style="display: block">Version 1.01</small>
         <p>
           <small>093.595.0000</small>
@@ -72,42 +60,43 @@
 </template>
 
 <script>
-const config = require("electron-json-config");
 const { clipboard } = require("electron");
-import TranslateData from "../../configs/translate-data";
+import xlsx from "node-xlsx";
+const path = require("path");
+const fs = require("fs");
+
 export default {
   name: "Translate",
-  data () {
+  data() {
     return {
       language: 0,
       languageData: [
         {
           value: 0,
-          text: "Hà Nội"
+          text: "Hà Nội",
         },
         {
           value: 1,
-          text: "Sài Gòn"
-        }
+          text: "Sài Gòn",
+        },
       ],
       translateData: [],
       inputText: "",
       resultText: "",
-      request: {}
+      request: {},
     };
   },
   methods: {
-    clearInput () {
+    clearInput() {
       this.inputText = "";
     },
-    clearResult () {
+    clearResult() {
       this.resultText = "";
     },
-    transalte () {
+    transalte() {
       this.resultText = "";
-      // get array text with \n
       const inputTexts = this.inputText.replace(/柜体/gm, "").split("\n");
-      inputTexts.map(inputText => {
+      inputTexts.map((inputText) => {
         const position = inputText.trim().search(/[0-9]/gm);
         let text, number;
         if (position !== -1) {
@@ -143,52 +132,19 @@ export default {
       });
       this.resultText = this.resultText.trim();
     },
-    copy () {
+    copy() {
       clipboard.writeText(this.resultText);
     },
-    // async checkToken () {
-    //   const authConfig = config.get("auth-config");
-    //   const diskLayout = await helper.getDiskLayout();
-    //   const serialNum = diskLayout[0].serialNum;
-    //   if (
-    //     helper.md5(serialNum + authConfig.token + "yunxiauto") !== authConfig.hashToken
-    //   ) {
-    //     router.push({
-    //       name: "Login"
-    //     });
-    //   }
-
-    //   await this.verifyDisk(authConfig, serialNum);
-    // },
-    // async verifyDisk (authConfig, serialNum) {
-    //   this.request.user_id = authConfig.user.id;
-    //   this.request.infor = serialNum;
-    //   this.request.token = authConfig.token;
-    //   const response = await AuthService.checkDisk(this.request)
-    //   if (response.status == 'error') {
-    //     config.deleteBulk(['auth-config']);
-    //     router.push({
-    //       name: "Login"
-    //     });
-    //   }
-    // }
   },
-  async mounted () {
-    // await this.checkToken();
-    // const response = await ConfigService.get();
-    // if (response.status == "success") {
-    //   if (response.data) {
-    //     const dataTranslate = JSON.parse(response.data.value).reduce(
-    //       (result, item) => {
-    //         result[item.key] = item.value;
-    //         return result;
-    //       },
-    //       {}
-    //     );
-    //     config.set("data-translate", dataTranslate);
-    //   }
-    // }
-    this.translateData = TranslateData
-  }
+  async mounted() {
+    // File path.
+    const workSheetsFromFile = xlsx.parse(path.join(__static, "/dich.xlsx"));
+    const dataExcel = workSheetsFromFile[0].data;
+    dataExcel.forEach((item) => {
+      this.translateData[item[0]] = item[1]
+    });
+
+    console.log(this.translateData)
+  },
 };
 </script>
